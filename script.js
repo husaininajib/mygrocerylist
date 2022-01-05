c84ac21f4546fc27e9210964d71388634c81a46b
husaininajib <husaininajib98@gmail.com>
2022-01-05 22:19:30 +0800

    added load event listener

--- script.js
+++ script.js
@@ -1,11 +1,44 @@
 const form = document.querySelector("form")
 const input = document.getElementById("text")
 const submitBtn = document.getElementById("submit")
-let list = []
+const listContainer = document.querySelector(".list-container")
+const list = []
 
-form.addEventListener("submit", (ev) => {
-    ev.preventDefault()
 
+window.addEventListener("load", () => {
+    updateList()
 })
 
-console.log(input.value)
+const mappedList = list.map(item => {
+        return item = `<li class="px-2 py-1 flex justify-between">
+                    <span>${item}</span>
+                    <ul class="edit-box flex gap-3">
+                        <li><i class="fas fa-edit text-green-600 cursor-pointer"></i></li>
+                        <li><i class="fas fa-trash-alt text-red-500 cursor-pointer"></i></li>
+                    </ul>
+                </li>`
+    }).join("")
+
+const updateList = () => {
+    form.addEventListener("submit", (ev) => {
+        ev.preventDefault()
+
+        if (input.value != "") {
+            list.push(input.value)
+            listContainer.innerHTML = mappedList
+        } else return
+
+        list.push(input.value)
+        input.value = ""
+        console.log(list)
+    })
+    console.log("hello")
+}
+
+    // console.log(list)
+
+
+
+
+
+
