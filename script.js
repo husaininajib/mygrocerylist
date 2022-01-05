d5b5f2f71e353e8bae15f7a6e99c00f6d0c03fc3
husaininajib <husaininajib98@gmail.com>
2022-01-05 23:48:35 +0800

    fixed bug in update list function

--- script.js
+++ script.js
@@ -9,33 +9,46 @@ window.addEventListener("load", () => {
     updateList()
 })
 
-const mappedList = list.map(item => {
-        return item = `<li class="px-2 py-1 flex justify-between">
+const updateList = () => {
+    form.addEventListener("submit", (ev) => {
+        ev.preventDefault()
+
+        if (input.value !== "") {
+            list.push(input.value)            
+        } else return
+
+
+
+        const mappedList = list.map(item => {
+        return `<li class="px-2 py-1 flex justify-between">
                     <span>${item}</span>
                     <ul class="edit-box flex gap-3">
                         <li><i class="fas fa-edit text-green-600 cursor-pointer"></i></li>
                         <li><i class="fas fa-trash-alt text-red-500 cursor-pointer"></i></li>
                     </ul>
                 </li>`
-    }).join("")
+        }).join("")
 
-const updateList = () => {
-    form.addEventListener("submit", (ev) => {
-        ev.preventDefault()
-
-        if (input.value != "") {
-            list.push(input.value)
-            listContainer.innerHTML = mappedList
-        } else return
-
-        list.push(input.value)
         input.value = ""
-        console.log(list)
+        listContainer.innerHTML = mappedList
+
+        const trashBtn = document.querySelectorAll(".fa-trash-alt")
+
+        trashBtn.forEach(btn => {
+            btn.addEventListener("click", () => {
+
+                list.pop(input.value)
+                console.log(list)
+            })
+        })
     })
-    console.log("hello")
 }
 
-    // console.log(list)
+
+// ======== DELETE LIST ==========
+
+
+
 
 
 
