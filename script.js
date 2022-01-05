e1cd19db93130a3fdaa2febd94f7fd85bd58fc98
husaininajib <husaininajib98@gmail.com>
2022-01-05 20:48:23 +0800

    completed interface

---
+++ script.js
@@ -0,0 +1,11 @@
+const form = document.querySelector("form")
+const input = document.getElementById("text")
+const submitBtn = document.getElementById("submit")
+let list = []
+
+form.addEventListener("submit", (ev) => {
+    ev.preventDefault()
+
+})
+
+console.log(input.value)
