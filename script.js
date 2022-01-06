b6a4f69466d22c886993d795ccf519be1e432441
husaininajib <husaininajib98@gmail.com>
2022-01-06 09:25:50 +0800

    added clear all function in js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const form = document.querySelector("form")
 const input = document.getElementById("text")
 const submitBtn = document.getElementById("submit")
 const listContainer = document.querySelector(".list-container")
+const clearAllBtn = document.getElementById("clear-all-btn")
 const list = []
 
 
@@ -20,7 +21,7 @@ const updateList = () => {
 
 
         const mappedList = list.map(item => {
-        return `<li class="px-2 py-1 flex justify-between">
+        return `<li class="px-2 py-1 flex justify-between list-box">
                     <span>${item}</span>
                     <ul class="edit-box flex gap-3">
                         <li><i class="fas fa-edit text-green-600 cursor-pointer"></i></li>
@@ -37,16 +38,31 @@ const updateList = () => {
         trashBtn.forEach(btn => {
             btn.addEventListener("click", () => {
 
+                const parentOfTrash = btn.parentElement
+                const editboxContainer = parentOfTrash.parentElement
+                const mainContainer = editboxContainer.parentElement
+                console.log(mainContainer)
                 list.pop(input.value)
+                mainContainer.style.display = "none"
                 console.log(list)
             })
         })
+
+        
     })
 }
 
 
 // ======== DELETE LIST ==========
 
+
+clearAllBtn.addEventListener("click", () => {
+    // listContainer.style.display = "none"
+})
+
+const num = [2,3,5,6,7,8]
+
+console.log(clearAllBtn.value)
 
 
 
